ad3dfcc4d3cad79af75e9c97678b40793ea29f8e
Keyology <keonimurray45@gmail.com>
2019-03-19 10:45:07 -0700

    developing api

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from jikanpy import Jikan
 app = Flask(__name__)
+jikan = Jikan()
 
 @app.route('/')
 def home():
     return "<h1>Anime recomendations</h1>"
+
+@app.route('/my-anime-user/<name>')
+def my_anime_username(name):
+    
+    # get profile info, same as above
+    my_anime_user = jikan.user(username=name, request='profile')
 
 if __name__ == "__main__":
     app.run()
