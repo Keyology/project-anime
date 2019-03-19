ec4c28cdf5f63750e5445e5833a1520cb271eae7
Keyology <keonimurray45@gmail.com>
2019-03-19 11:51:14 -0700

    added comments

--- app.py
+++ app.py
@@ -9,16 +9,22 @@ jikan = Jikan()
 def home():
     return "<h1>Anime recomendations</h1>"
 
+
 @app.route('/my-anime-user', methods=["GET", "POST"])
+# this route will get user myAniList profile info
 def my_anime_username():
     print("route has been hit")
-    # get profile info, same as above
+    # Get the username from the url
     name_param = request.args.get('name')
     print("This is name params", name_param)
+    # convert the username to a string 
     name_param = str(name_param)
+    # request user anilist profile info 
     my_anime_user = jikan.user(username=name_param, request='profile')
+    #convert response to json
     response = jsonify(my_anime_user)
     print("This is the response",response)
+    #return the response
     return response
 
 if __name__ == "__main__":
